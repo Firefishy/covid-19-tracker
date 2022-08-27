d7aee538811d8ba523d137efca89cc7d67853cfb
Grant <github@firefishy.com>
2022-08-28 01:00:15 +0300

    Update Map.js
    `{s}.` is no longer recommended now that we support HTTP/2 + HTTP/3.

--- src/Map.js
+++ src/Map.js
@@ -8,7 +8,7 @@ function Map({ countries, casesType, center, zoom }) {
     <div className="map">
       <MapContainer center={center} zoom={zoom}>
         <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
         {showDataOnMap(countries, casesType)}
